7cbfd88b338268a51817ab832caa45edbc311e8b
Dewa <108676698+DewaMadeWira@users.noreply.github.com>
2024-09-18 08:53:21 +0700

    upgrade astro, change width, add dawarehouse page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {
-            colors: {
-                black: '#010001',
-                white: '#FFFFFF',
-                blue: '#0A00F7',
-                orange: '#F75500',
-            },
-            // fontFamily: {
-            //     dm: ['DM Sans', 'sans-serif'],
-            //     jakarta: ['Plus Jakarta Sans', 'sans-serif'],
-            // },
-        },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#010001",
+        white: "#FFFFFF",
+        blue: "#0A00F7",
+        orange: "#F75500",
+      },
+      // fontFamily: {
+      //     dm: ['DM Sans', 'sans-serif'],
+      //     jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+      // },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
